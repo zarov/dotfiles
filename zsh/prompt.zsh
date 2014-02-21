b7edefc8e90202b7a492cb234660e7f881dca449
Adrien <berthet.a@gmail.com>
2014-02-21 13:09:39 +0100

    Updates zsh config for OS X.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,22 +1,88 @@
 #!/usr/bin/env zsh
 
-autoload -U promptinit
-promptinit
+autoload -U promptinit && promptinit
+autoload -U colors && colors
 
-autoload -U colors
-colors
+# thank you @holman & @ehrenmurdick
+if (( $+commands[git] ))
+then
+  git="$commands[git]"
+else
+  git="/usr/bin/git"
+fi
 
-# Colors
-for color in red green yellow blue white magenta cyan; do
-	eval PR_$color='%{$terminfo[bold]$fg[$color]%}'
-	eval PR_LIGHT_$color='%{$fg[$color]%}'
-done
+git_branch() {
+  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+}
 
-#PR_NO_COLOR='%{$terminfo[sgr0]%}'
+git_dirty() {
+  st=$($git status 2>/dev/null | tail -n 1)
+  if [[ $st == "" ]]
+  then
+    echo ""
+  else
+    if [[ "$st" =~ ^nothing ]]
+    then
+      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+    else
+      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+    fi
+  fi
+}
 
-# Prompt & RightPrompt
-export PS1="[$PR_LIGHT_magenta%~$PR_LIGHT_white]%# "
-export RPS1="$PR_cyan%B%T%b"
+git_prompt_info () {
+ ref=$($git symbolic-ref HEAD 2>/dev/null) || return
+# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
+ echo "${ref#refs/heads/}"
+}
+
+unpushed () {
+  $git cherry -v @{upstream} 2>/dev/null
+}
+
+need_push () {
+  if [[ $(unpushed) == "" ]]
+  then
+    echo " "
+  else
+    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+  fi
+}
+
+ruby_version() {
+  if (( $+commands[rbenv] ))
+  then
+    echo "$(rbenv version | awk '{print $1}')"
+  fi
+
+  if (( $+commands[rvm-prompt] ))
+  then
+    echo "$(rvm-prompt | awk '{print $1}')"
+  fi
+}
+
+rb_prompt() {
+  if ! [[ -z "$(ruby_version)" ]]
+  then
+    echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
+  else
+    echo ""
+  fi
+}
+
+directory_name() {
+  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+}
+
+export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+set_prompt () {
+  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+}
+
+precmd() {
+  title "zsh" "%m" "%55<...<%~"
+  set_prompt
+}
 
 # UTF8 FTW
 export LC_ALL=fr_FR.UTF-8
