14daab9f9915a75a0f64b3bd25cb3829e9feacaf
Adrien Berthet <adrien.berthet@ign.fr>
2019-06-13 14:52:03 +0200

    add a git-branch-clean function to zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -22,3 +22,16 @@ function mcd() { mkdir -p $1 && cd $1 }
 function note() { vim -c "Goyo" ~/Workspace/notes/$*.md }
 function notels() { ls -c ~/Workspace/notes/ | grep "$*" }
 function cjd() { cd ~/Documents/*/*/$1* } # https://johnnydecimal.com/concepts/working-at-the-terminal/
+
+function git-branch-clean() {
+	for BRANCH in `git branch | grep -v HEAD`;
+	do
+		git show --format="%C(cyan)$BRANCH%Creset has last been update %C(yellow)%cr%Creset by %Cgreen%an%Creset: %n%s." -s $BRANCH
+		read "REPLY?Delete this branch ? y/[N] "
+		if [[ "$REPLY" =~ ^[Yy]$ ]];
+		then
+			git branch -D $BRANCH
+		fi
+		echo  \\n
+	done
+}
